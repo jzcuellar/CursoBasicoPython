6b345a41e5924cc05066bf605080227d46d98674
Jefferzon <jefferzon.cuellar@gmail.com>
2021-10-28 23:17:36 -0500

    Solucion y una alternativa interesante de Solucion

--- Ejercicios de Practica Python/who_likes_it.py
+++ Ejercicios de Practica Python/who_likes_it.py
@@ -1,3 +1,28 @@
+"""
+Description:
+
+You probably know the "like" system from Facebook and other pages. 
+People can "like" blog posts, pictures or other items. We want to 
+create the text that should be displayed next to such an item.
+
+Implement the function which takes an array containing the names
+of people that like an item. It must return the display text as 
+shown in the examples:
+
+
+[]                                -->  "no one likes this"
+["Peter"]                         -->  "Peter likes this"
+["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+
+
+"""
+
+## Ejecutar Solo 1 Funcion que son soluciones unicas del problema 
+
+##  SOLUCION #1 - Solucion Creada Por mi 
+
 def likes(names):
     if len(names)<1:
         return 'no one likes this'
@@ -10,11 +35,35 @@ def likes(names):
     else:
         return (f'{names[0]}, {names[1]} and {len(names)-2} others like this')
 
-names=["Alex", "Jacob", "Mark", "Max"]
+##  SOLUCION #2 - https://www.codewars.com/kata/reviews/564425cc55d0e45b8c000087/groups/587d40312ec7b84b2f00102f
 
-likes(names)
+def likes(names):
 
-##Probable Forma de Solucion
-# for i in range(len(names)):
-#     print(names[i], 'and', end=' ')
-# print('likes this')
+    d = { #1
+        0: "no one likes this",
+        1: "{} likes this", #2
+        2: "{} and {} like this",
+        3: "{}, {} and {} like this",
+        4: "{}, {} and {others} others like this"
+        }
+    length = len(names)
+
+    return d[min(4, length)].format(*names, others = length - 2) #4 #5 #6
+    
+    ##1  Make a dictionary d of all the possible answers. 
+    #    Keys are the respective number of people who liked it.
+    
+    ##2  {} indicate placeholders. They do not need any numbers but are simply 
+    #    replaced/formatted in the order the arguments in names are given to format
+    # 
+    ##3  {others} can be replaced by other value; below the argument "others = length - 2"
+    #    is passed to str.format()
+
+    ##4  d[min(4, length)] insures that the appropriate string is called from the dictionary
+    #    and subsequently returned. Min is necessary as len(names) may be > 4
+    #    funtion min(list or dictionary) returns the minimum value
+
+    ##5  The * in *names ensures that the list names is blown up and that format is called
+    #    as if each item in names was passed to format individually, i. e.
+    #
+    ##6  format(names[0], names[1], .... , names[n], others = length - 2
