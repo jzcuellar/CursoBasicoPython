1f5bf6ea14a133ae82449aaa5c7c56736250afc7
Jefferzon <jefferzon.cuellar@gmail.com>
2023-01-02 18:04:23 -0500

    Cambio en nombre de los archivos, mejor organizacion

--- Ejercicios de Practica Python/Disemvowel_Trolls.py
+++
@@ -1,9 +0,0 @@
-def disemvowel(string_):    
-    vowels=['a','e','i','o','u']
-    for i in len(string_):
-        string_.replace(vowels[1],'')
-    return string_
-
-
-string_='This website is for losers LOL'
-print(disemvowel(string_))
